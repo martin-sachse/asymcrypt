868298a017246d5261f7d94930f72166ff221a3b
martin-sachse <martin.sachse@gmail.com>
2011-01-30 23:49:54 +0100

    added decryption

--- participate.js
+++ participate.js
@@ -3,87 +3,69 @@
 */
 function evaluateTable()  {
 	
-	var pollData = new Array();
-	var checkedRadio = $('#polltable  input:radio').filter(':checked').each( function(index) {
-		pollData[index] = $(this).attr('id'); 
-	});
+  var pollData = new Array();
+  var checkedRadio = $('#polltable  input:radio').filter(':checked').each( function(index) {
+    pollData[index] = $(this).attr('id'); 
+  });
 
-	return pollData;
+  return pollData;
 }
 
 /**
 *  gets the decrypted json-database, builds and insert the table rows, adds the result even with undecrypted votes
 */
-function showParsedTable(dbVotes) {
-	var voteTR = $('#add_participant');
-	var possibilities = new Array();
-	
-	//reads the characters that symbolize yes, maybe, no, so they are synchronous on the whole page
-	possibilities['ayes'] = $('.input-ayes:first label').text();
-	possibilities['cno'] = $('.input-cno:first label').text();
-	possibilities['bmaybe'] = $('.input-bmaybe:first label').text();
+function parseRow(rowId, decodedText) {
+  var possibilities = new Array();
+  //reads the characters that symbolize yes, maybe, no, so they are synchronous on the whole page
+  possibilities['ayes'] = $('.input-ayes:first label').text();
+  possibilities['cno'] = $('.input-cno:first label').text();
+  possibilities['bmaybe'] = $('.input-bmaybe:first label').text();
   
   //hides the voting part of the table
-  voteTR.hide();
-  
-  //parses the votes of each participant into an array with the construction participants[participant0-participantX][vote0-voteY]
-	var participants = new Array();	
-	for (participant in dbVotes) {
-		participants[participant] = new Array();
-		var votes = JSON.parse(dbVotes[participant]);
-		for (vote in votes) {
-			participants[participant][participants[participant].length] = JSON.parse(votes[vote]);
-		}
-   }
+  $('#add_participant').hide();
   
   //reads the latest dates/poll-items (@todo rename dates to something more neutral), so changes in the poll will not affect earlier votes
-	var dates = new Array();
-	var yesVotes = new Array();
-	var checkedRadio = $('#polltable input:radio').filter(':checked').each( function(index) {
-		dates[dates.length] = ($(this).attr('id').split('_'))[3]; 
-		yesVotes[dates[dates.length-1]] = 0;
-	});
-	
-  //builds the table row for every encrypted participant, counts the yes votes and insert it between the separator_top and the now hidden voting part
-	var participantTRs = new Array();
-	for(participant in participants) {
-		var tr = '<tr class="participantrow"><td colspan="2" class="name"><span id="' + participant + '">' + participant + '</span></td>';
-		for(pollDate in dates) {
-			var dateExists = false;
-			for(vote in participants[participant]) {
-				if(dates[pollDate] == participants[participant][vote][0]) {
-					var decision = participants[participant][vote][1];
-					if(decision == 'ayes') {
-					  yesVotes[dates[pollDate]]++;
-					}
-					tr += '<td class="' + decision + '" title="' + participant + ': ' + dates[pollDate].replace(".", " ") + '">' + possibilities[decision] + '</td>';//maybe date when entered 
-					dateExists = true;
-				}
-			}
-			if(!dateExists) {
-				tr += '<td class="undecided" title="' + participant + ': ' + dates[pollDate].replace(".", " ") + '">&ndash;</td>';
-			}
-		}
-		tr += '<td class="date" style="text-align:center">encrypted</td></tr>';
-		$(tr).insertBefore(voteTR);
-	}
-	
-	//updates the summary row, colors and gives the title like in the original
-	var numberParticipants = $('td.name').length-1
-	for(pollDate in dates) {
-	  var pollItemSummary = $('#summary > td:eq('+(1+parseInt(pollDate))+')');
-	  var pollItemSum = parseInt(pollItemSummary.html())+yesVotes[dates[pollDate]];
-	  
-	  pollItemSummary.html(pollItemSum).css('width', '44px');
-	  
-	  if(numberParticipants == pollItemSum && numberParticipants != 0) {
-	    pollItemSummary.css({'background-color' : 'rgb(0,0,0)', 'color' : 'rgb(255,255,255)'}).attr('title', '100%');
-
-	  } else {
-	    pollItemSummary.css({'background-color' : 'rgb(100,100,100)', 'color' : 'rgb(155,155,155)'}).attr('title', '0%-100%');
+  var dates = new Array();
+  var yesVotes = new Array();
+  var checkedRadio = $('#polltable input:radio').filter(':checked').each( function(index) {
+	dates[dates.length] = ($(this).attr('id').split('_'))[3];
+	yesVotes[dates.length-1] = 0;
+  });
+  
+  var decoded = JSON.parse(decodedText);
+  var decryptedRow = '<tr><td class="name" colspan="2">' + decoded[0] + '</td>';
+  for(pollDate in dates) {
+    var dateExists = false;
+	for(var i = 1; i < decoded.length; i++) {
+	  if(dates[pollDate] == decoded[i][0]) {
+	    if(decoded[i][1] == 'ayes') {
+	      yesVotes[pollDate] = 1;
+	    }
+        decryptedRow += '<td class="' + decoded[i][1] + '" title="' + decoded[i][0] + ': ' + dates[pollDate].replace(".", " ") + '">' + possibilities[decoded[i][1]] + '</td>';//maybe date when entered 
+        dateExists = true;
 	  }
 	}
-	$('#summary').show();
+	if(!dateExists) {
+      decryptedRow += '<td class="undecided" title="' + participant + ': ' + dates[pollDate].replace(".", " ") + '">&ndash;</td>';
+	}
+  }
+  decryptedRow += '<td class="date" style="text-align:center">decrypted</td></tr>';
+ 
+  $('#'+rowId).replaceWith(decryptedRow);
+		
+  //updates the summary row, colors and gives the title like in the original
+  var numberParticipants = $('td.name').length-1;
+  for(pollDate in dates) {
+    var pollItemSummary = $('#summary > td:eq('+(1+parseInt(pollDate))+')');
+	var pollItemSum = parseInt(pollItemSummary.html())+yesVotes[pollDate];
+	  
+	pollItemSummary.html(pollItemSum).css('width', '44px');
+	if(numberParticipants == pollItemSum && numberParticipants != 0) {
+	  pollItemSummary.css({'background-color' : 'rgb(0,0,0)', 'color' : 'rgb(255,255,255)'}).attr('title', '100%');
+	} else {
+	  pollItemSummary.css({'background-color' : 'rgb(100,100,100)', 'color' : 'rgb(155,155,155)'}).attr('title', '0%-100%');
+	}
+  }
 }
 
 function randomRowName(dbVotes) {
@@ -151,14 +133,13 @@ $(document).ready(function() {
 		var db = JSON.parse(r);
 		var keyId = db.keyId;
 		var publicKey = db.key.replace(/\s+/g, '+');
-		$('#summary').hide();
 		$('.sortsymb').hide();
 		if(JSON.stringify(db.votes) != '{}') {
 			$(getEncryptedRow('encryptedData', 'Encrypted Polldata')).click( function() {
 				var encryptedRows = "";
 				var i = 0;
 				for (participant in db.votes) {
-					encryptedRows += getEncryptedRow(i++, '<textarea rows="1" cols="1" style="width: 95%; margin-top:5px">' + JSON.parse(db.votes[participant]) + '</textarea>');
+					encryptedRows += getEncryptedRow('encRow' + i++, '<textarea rows="1" cols="1" style="width: 95%; margin-top:5px">' + JSON.parse(db.votes[participant]) + '</textarea>');
 				}
 				$('#encryptedData').replaceWith(encryptedRows);
 			}).insertBefore('#add_participant');
@@ -176,6 +157,11 @@ $(document).ready(function() {
 			$('#add_participant, #encryptedData').hide();
 			return false;
   		}); 
+		$('tr[id^="encRow"] textarea').live('change', function() {
+			var rowId = $(this).parent().parent().attr('id');
+			var decodedText = $(this).val();
+			parseRow(rowId, decodedText);
+		});
   	  }
     });
   }
