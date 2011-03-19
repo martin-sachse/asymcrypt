0a140d557b82ca5360a7e20848a91e8a99e48fc4
martin-sachse <martin.sachse@gmail.com>
2011-03-19 16:55:24 +0100

    Lizenzen eingefügt

--- access_control.js
+++ access_control.js
@@ -1,4 +1,26 @@
 /**
+############################################################################
+# Copyright 2011 Benjamin Kellermann, Martin Sachse, Oliver Hoehne, 	     #
+# Robert Bachran         				  		                                     #
+#                                                                          #
+# This file is part of dudle.                                              #
+#                                                                          #
+# Dudle is free software: you can redistribute it and/or modify it under   #
+# the terms of the GNU Affero General Public License as published by       #
+# the Free Software Foundation, either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# Dudle is distributed in the hope that it will be useful, but WITHOUT ANY #
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public     #
+# License for more details.                                                #
+#                                                                          #
+# You should have received a copy of the GNU Affero General Public License #
+# along with dudle.  If not, see <http://www.gnu.org/licenses/>.           #
+############################################################################
+*/
+
+/**
 * creates asymcrypt_data.yaml and writes keyId and publicKey into it
 */
 function saveData(publicKey, keyOwner) {
